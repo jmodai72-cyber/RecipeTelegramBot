42ac04cf9c14f04c0ad69205bc90dc403dc93899
jmodai72-cyber <jmodai72@gmail.com>
2025-10-24 13:15:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -321,6 +321,7 @@ def webhook():
     if request.headers.get('content-type') == 'application/json':
         try:
             json_string = request.get_data().decode('utf-8')
+            print(f"== Получен апдейт от Telegram: {json_string}")  # <-- ЭТУ СТРОКУ Я ДОБАВИЛ!
             update = telebot.types.Update.de_json(json_string)
             bot.process_new_updates([update])
             return '', 200
